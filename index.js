9d087b63dc6bacfb01c39fc8b53b52b38ca19922
Ankit Mod <ankit300mod@gmail.com>
2024-07-29 13:49:07 +0530

    data

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const btn = document.getElementById('btn');
-    const textarea = document.getElementById('textarea');
-
-    if (btn && textarea) {
-        btn.addEventListener('click', () => {
-            const userInput = textarea.value;
-            const speech = new SpeechSynthesisUtterance();
-            speech.text = userInput;
-            window.speechSynthesis.speak(speech);
-        });
-       
-    }
-
-    else {
-        console.error('Button or textarea not found');
-    }
-});
+const speak = async() => {
+    const utterance = new SpeechSynthesisUtterance();
+    const dialogue = document.getElementById('textarea').value;
+    utterance.text = dialogue;
+   
+    speechSynthesis.speak(utterance);
+    
+}
